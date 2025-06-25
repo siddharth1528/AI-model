5a7f4244deb80484a96ebb18c667b0bc43785782
siddharth singh <70317457+siddharth1528@users.noreply.github.com>
2025-06-25 12:37:57 +0530

    streamlit_test_2.py

--- streamlit_test_2.py
+++ streamlit_test_2.py
@@ -531,7 +531,7 @@ with input_col:
         user_query = st.text_input("💬 Ask your question:", placeholder="e.g. List all FCDs in WIP", key="user_input")
         submitted = st.form_submit_button("O_O 🔍 Ask")
 with img_col:
-    st.image("https://cdn-icons-png.flaticon.com/512/10160/10160256.png", width=80)
+    st.image("https://cdn-icons-png.flaticon.com/512/14201/14201937.png", width=80)
 
 st.markdown('</div>', unsafe_allow_html=True)
 
